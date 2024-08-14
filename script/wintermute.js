02736f7ed8a4ab74f1ed7927846cc2f7e8d2410c
Tess Walker <tesshwalker@macbook-air.lan>
2024-08-14 14:47:15 -0400

    for you (me)

--- script/wintermute.js
+++ script/wintermute.js
@@ -8,7 +8,7 @@ function wintermute() {
     document.getElementById('blackOut').style = 'display: inline';
     wait(1770).then(() => document.getElementById('connected').style ='display: inline');
     wait(1790).then(() => document.getElementById('connected').style ='display: none');
-    wait(1815).then(() => document.getElementById('connected').style ='display: inline');
+    wait(1821).then(() => document.getElementById('connected').style ='display: inline');
     wait(2605).then(() => document.getElementById('connected').style ='display: none');
     wait(2632).then(() => window.location.reload()); /* allows back arrow to not display connected */
     wait(2632).then(() => window.location = "desktop.html");
