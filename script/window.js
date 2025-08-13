41dd6e1bd01827857558663332dd316be0c707fc
Raine <rainehwalker@proton.me>
2025-08-13 17:19:10 -0400

    fucntions... thinking

--- script/window.js
+++ script/window.js
@@ -1,7 +1,10 @@
 function minimizeWindow() {
-    
+    const window = document.getElementById('blockWrapper');
+    window.style.opacity = '0';
 }
 
+/* MINIMIZE WINDOW ONTO TOP BAR TO ALLIGN WITH TERMINAL PERHAPS */
+
 function maximizeWindow() {
-    
+
 }
