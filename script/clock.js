c8eec4eb6bb7bbc7d9047524c49564b51f3da01e
Raine <rainehwalker@proton.me>
2026-04-01 09:56:25 -0400

    raine-y-day

--- script/clock.js
+++ script/clock.js
@@ -1,3 +1,14 @@
-function timeFreak() {
-    
+function clock() {
+    const clockDisplay = document.getElementById("clockText");
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    m = checkTime(m);
+    clockDisplay.innerHTML = h + ":" + m;
+    setTimeout(startTime, 1000);
+}
+
+function checkTime(i) {
+    if (i < 10) { i = "0" + i };
+    return i;
 }
